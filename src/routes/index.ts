5b8a9a054fba4d0b2d4a9ad831a06a48d9db77bb
Noah Brenner <noah@noahbrenner.com>
2021-05-06 18:41:31 -0700

    Remove unneeded boilerplate code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
 
 import { categoryRouter } from "./categories";
-import { userRouter } from "./users";
 
-// Init router and path
 const router = Router();
 
-// Add sub-routes
 router.use("/categories", categoryRouter);
-router.use("/users", userRouter);
 
-// Export the base-router
 export { router as baseRouter };
