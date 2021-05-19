a48e9e8e6eb8b30d8511c99835b085272045a062
Noah Brenner <noah@noahbrenner.com>
2021-05-18 19:36:50 -0700

    Retry connecting to the DB more times
    In CI, we're getting an intermittent ECONNREFUSED error. It seems like a
    race condition with the DB sometimes not being available when the
    migrate script tries to connect to it.

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,6 @@ import { createPool } from "slonik";
 
 export { sql } from "slonik";
 
-export const pool = createPool(process.env.POSTGRES_CONNECTION_STRING ?? "");
+export const pool = createPool(process.env.POSTGRES_CONNECTION_STRING ?? "", {
+  connectionRetryLimit: 10,
+});
